7ca77723d2d332746b0484a0ec940a2b337ca4a9
Ammaar Patel <ammaarpatel99@gmail.com>
2021-01-01 15:58:19 +0000

    Use nav container as core component
    Place nav container as sole element in app component and place router-outlet inside it

--- projects/public/src/app/nav-container/nav-container.module.ts
+++ projects/public/src/app/nav-container/nav-container.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {RouterModule} from '@angular/router';
 
 
 
@@ -19,7 +20,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    RouterModule
   ],
   exports: [NavContainerComponent]
 })
