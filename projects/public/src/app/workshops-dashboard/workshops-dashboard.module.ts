63672fafcb332b533b366e91259cca2b17013faa
Ammaar Patel <ammaarpatel99@gmail.com>
2021-01-25 14:55:34 +0000

    add missing export
    previously wasn't exporting component (compiler was probably adding in export)

--- projects/public/src/app/workshops-dashboard/workshops-dashboard.module.ts
+++ projects/public/src/app/workshops-dashboard/workshops-dashboard.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
         MatCardModule,
         RouterModule,
         MatButtonModule
-    ]
+    ],
+  exports: [WorkshopsDashboardComponent]
 })
 export class WorkshopsDashboardModule { }
