bf2ae6c3789d40d8e1d408976f5ff0f52938c846
Ammaar Patel <ammaarpatel99@gmail.com>
2021-02-06 13:47:17 +0000

    refine account module

--- projects/public/src/app/account/account-routing.module.ts
+++ projects/public/src/app/account/account-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AccountComponent} from './account/account.component';
+import {AccountComponent} from './account.component';
 
 
 const routes: Routes = [
