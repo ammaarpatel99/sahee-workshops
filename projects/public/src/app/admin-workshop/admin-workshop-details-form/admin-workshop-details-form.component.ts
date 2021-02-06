4d3c42fb84a05f7833a103b62449be53bb1b1a26
Ammaar Patel <ammaarpatel99@gmail.com>
2021-02-06 16:57:52 +0000

    refine workshop details form

--- projects/public/src/app/admin-workshop/admin-workshop-details-form/admin-workshop-details-form.component.ts
+++ projects/public/src/app/admin-workshop/admin-workshop-details-form/admin-workshop-details-form.component.ts
@@ -1,15 +1,16 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {AdminWorkshop, AdminWorkshopDoc} from '../../../../../../functions/src/firebase-helpers/firestore-interfaces';
-import {Observable, of, ReplaySubject, Subscription} from 'rxjs';
+import {Component, Input} from '@angular/core';
+import {AdminWorkshop, AdminWorkshopDoc} from '@firebase-helpers';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {MatFormFieldAppearance} from '@angular/material/form-field';
-import {finalize, map, switchMap, switchMapTo, take, tap} from 'rxjs/operators';
+import {map, switchMap, switchMapTo} from 'rxjs/operators';
 import {ActivatedRoute, Router} from '@angular/router';
 import firebase from 'firebase/app';
 import Timestamp = firebase.firestore.Timestamp;
 import {AdminWorkshopsService} from '../../services/admin-workshops/admin-workshops.service';
 
+
 type WorkshopDetails = Exclude<keyof AdminWorkshop, 'id'|'datetime' >;
+
 
 const DISPLAY_FIELDS: {
   title: string; name: WorkshopDetails, type: 'text'|'datetime-local'|'text-area'; required: boolean; tooltip: string;
@@ -28,22 +29,58 @@ const DISPLAY_FIELDS: {
     and then embed, and then copy the link (not the whole block of code, just the link in the speech marks next to src=).`}
 ];
 
+
 @Component({
   selector: 'app-admin-workshop-details-form',
   templateUrl: './admin-workshop-details-form.component.html',
   styleUrls: ['./admin-workshop-details-form.component.scss']
 })
-export class AdminWorkshopDetailsFormComponent implements OnInit, OnDestroy {
-  readonly displayFields = DISPLAY_FIELDS;
-  private readonly _editing$ = new ReplaySubject<boolean>(1);
-  readonly editing$ = this._editing$.asObservable();
+export class AdminWorkshopDetailsFormComponent {
   /**
-   * An observable that produces a Workshop instance when editing said workshop, and undefined
-   * when creating a new workshop. The observable should be multicast and have a buffer (i.e.
-   * a ReplaySubject(1) or an observable piped through shareReplay(1)).
+   * The fields of a workshop that can be edited, and details for displaying them.
    */
-  @Input() workshop$!: Observable<AdminWorkshop|undefined>;
+  readonly displayFields = DISPLAY_FIELDS;
 
+
+  /**
+   * Internal field for {@link editing}.
+   * @private
+   */
+  private _editing = false;
+  /**
+   * Whether the form is in editing mode or not.
+   */
+  get editing(): boolean {
+    return this._editing;
+  }
+  set editing(editing: boolean) {
+    if (editing) this.form.enable();
+    else this.form.disable();
+    this._editing = editing;
+  }
+
+
+  /**
+   * Internal field for {@link workshop}.
+   * @private
+   */
+  private _workshop: AdminWorkshop | null = null;
+  /**
+   * The workshop being edited, or null if the form is being used to create one.
+   */
+  @Input()
+  set workshop(workshop: AdminWorkshop | null) {
+    this.resetWorkshopDetailsForm(true);
+    this._workshop = workshop;
+  }
+  get workshop(): AdminWorkshop | null {
+    return this._workshop;
+  }
+
+
+  /**
+   * The form for editing the details of the workshop.
+   */
   readonly form = new FormGroup({
     name: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required),
@@ -54,142 +91,155 @@ export class AdminWorkshopDetailsFormComponent implements OnInit, OnDestroy {
     newSignupEmail: new FormControl('', Validators.required)
   });
 
+
+  /**
+   * A getter for form controls in {@link form}.
+   * @param field - The name of the form control to get.
+   */
   getFormControl(field: WorkshopDetails): FormControl {
-    return this.form.get(field) as FormControl;
+    const control = this.form.get(field);
+    if (!control || !(control instanceof FormControl)) {
+      throw new Error(`There is no form control with name: ${field}.`);
+    }
+    return control;
   }
 
+
+  /**
+   * How to display the form field.
+   * @param field - The name of the field.
+   * @returns - The value of MatFormField appearance.
+   */
   getFormFieldAppearance(field: WorkshopDetails): MatFormFieldAppearance {
     return this.getFormControl(field).pristine ? 'fill' : 'standard';
   }
 
+
+  /**
+   * The error message for a field from {@link form}.
+   * @param field - The name of the field.
+   * @returns - The error message to display.
+   */
   getFieldErrorMessage(field: WorkshopDetails): string {
     const control = this.getFormControl(field);
     if (control.hasError('required')) return 'This field is required';
     else return '';
   }
 
-  async resetWorkshopDetailsForm(initialising: boolean = false): Promise<void> {
-    if (!initialising && (this.form.pristine || this.form.disabled)) {
+
+  /**
+   * Resets the form. If the form is disabled, this throws an error.
+   * @param force - A boolean flag that defaults to false.
+   * If it set to true it resets the form regardless of its state.
+   */
+  resetWorkshopDetailsForm(force: boolean = false): void {
+    if (!force && this.form.disabled) {
       throw new Error(`Can't reset workshop details form.`);
     }
-    try {
-      const workshop = await this.workshop$.pipe(take(1)).toPromise();
-      if (!workshop) {
-        this.form.reset();
-        this._editing$.next(true);
-        return;
-      }
-      this.form.reset({...workshop, jsDate: workshop.jsDate.toISOString().slice(0, -1)});
-      this._editing$.next(false);
-    } finally {
+    if (!this.workshop) {
+      this.form.reset();
+      this.editing = true;
+      return;
     }
+    this.form.reset({...this.workshop, jsDate: this.workshop.jsDate.toISOString().slice(0, -1)});
+    this.editing = false;
   }
 
-  edit(): void {
-    this._editing$.next(true);
-  }
 
+  /**
+   * Submits the form. This means either creating or the workshop or updating it.
+   */
   async submit(): Promise<void> {
     if (this.form.invalid || this.form.pristine || this.form.disabled) {
       throw new Error(`Can't submit workshop details form.`);
     }
-    try {
-      const currentWorkshop = await this.workshop$.pipe(take(1)).toPromise();
-      if (!currentWorkshop) {
-        await this.createWorkshop();
-      } else {
-        await this.updateWorkshop(currentWorkshop.id);
-      }
-    } finally {
+    if (!this.workshop) {
+      await this.createWorkshop();
+    } else {
+      await this.updateWorkshop(this.workshop.id);
     }
   }
 
-  async delete(): Promise<void> {
+
+  /**
+   * Deletes the workshop.
+   */
+  delete(): Promise<void> {
     if (this.form.enabled) {
       throw new Error(`Can't delete workshop details.`);
     }
-    const currentWorkshop = await this.workshop$.pipe(take(1)).toPromise();
-    if (!currentWorkshop) {
-      return;
-    }
-    this.adminWorkshopsService.delete$(currentWorkshop.id).pipe(
-      switchMapTo(this.router.navigate(['..'], {relativeTo: this.route}))
-    ).subscribe();
+    if (!this.workshop) throw new Error(`Can't delete the workshop as it doesn't exist.`);
+    return this.adminWorkshopsService.delete$(this.workshop.id).pipe(
+      switchMapTo(this.router.navigate(['..'], {relativeTo: this.route})),
+      map(res => {
+        if (!res) throw new Error(`Couldn't navigate away after deleting workshops.`);
+      })
+    ).toPromise();
   }
 
+
+  /**
+   * Updates the workshop.
+   * @param workshopID - The ID of the workshop to update.
+   * @private
+   */
   private async updateWorkshop(workshopID: string): Promise<void> {
     const data: Partial<AdminWorkshopDoc> = {};
+
     const getValue = (name: WorkshopDetails): string | undefined => {
       if (this.getFormControl(name).dirty) return this.getFormControl(name).value;
       return undefined;
     };
+
     const setValue = (name: Exclude<WorkshopDetails, 'jsDate'>): void => {
       const value = getValue(name);
       if (value !== undefined) data[name] = value;
     };
+
     const fields: Exclude<WorkshopDetails, 'jsDate'>[] =
       ['name', 'description', 'newSignupEmail', 'videoCallLink', 'feedbackLink', 'recordingLink'];
     for (const field of fields) setValue(field);
+
     const datetime = getValue('jsDate');
     if (datetime) data.datetime = Timestamp.fromDate(new Date(datetime));
+
     return this.adminWorkshopsService.update$(workshopID, data).toPromise();
   }
 
-  private async createWorkshop(): Promise<void> {
+
+  /**
+   * Creates a workshop using the data from {@link form}.
+   * @private
+   */
+  private createWorkshop(): Promise<void> {
     const data: AdminWorkshopDoc = {
       name: this.getFormControl('name').value,
       description: this.getFormControl('description').value,
       datetime: Timestamp.fromDate(new Date(this.getFormControl('jsDate').value)),
       newSignupEmail: this.getFormControl('newSignupEmail').value
     };
+
     const setValue = (field: Exclude<WorkshopDetails, 'jsDate'>): void => {
       const value = this.getFormControl(field).value;
       if (value) data[field] = value;
     };
+
     setValue('videoCallLink');
     setValue('recordingLink');
     setValue('feedbackLink');
-    const id = await this.adminWorkshopsService.create$(data).toPromise();
-    await this.router.navigate(['..', id], {relativeTo: this.route});
+
+    return this.adminWorkshopsService.create$(data).pipe(
+      switchMap(id => this.router.navigate(['..', id], {relativeTo: this.route})),
+      map(res => {
+        if (!res) throw new Error(`Couldn't navigate away after creating workshop.`);
+      })
+    ).toPromise();
   }
+
 
   constructor(
     private readonly router: Router,
     private readonly route: ActivatedRoute,
     private readonly adminWorkshopsService: AdminWorkshopsService
-  ) {
-    this.subscriptions.push(
-      this.manageFormEnabledState$().subscribe()
-    );
-  }
-
-  private manageFormEnabledState$(): Observable<void> {
-    return this.editing$.pipe(
-      map((editing: boolean): void => {
-        if (editing) this.form.enable();
-        else this.form.disable();
-      })
-    );
-  }
-
-  ngOnInit(): void {
-    this.subscriptions.push(
-      this.watchWorkshopAndUpdateForm$().subscribe()
-    );
-  }
-
-  private watchWorkshopAndUpdateForm$(): Observable<void> {
-    return this.workshop$.pipe(
-      tap(async _ => {
-        await this.resetWorkshopDetailsForm(true);
-      }),
-      map(_ => {})
-    );
-  }
-
-  private readonly subscriptions: Subscription[] = [];
-  ngOnDestroy(): void {
-    for (const s of this.subscriptions) if (!s.closed) s.unsubscribe();
-  }
-
+  ) { }
 }
