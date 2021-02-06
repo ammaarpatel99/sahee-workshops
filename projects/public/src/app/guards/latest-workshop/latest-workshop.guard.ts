b044931d6a9049e135a895b419896e12ca6384b6
Ammaar Patel <ammaarpatel99@gmail.com>
2021-02-06 11:21:09 +0000

    refine latest-workshop guard

--- projects/public/src/app/guards/latest-workshop/latest-workshop.guard.ts
+++ projects/public/src/app/guards/latest-workshop/latest-workshop.guard.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
-import {map, take} from 'rxjs/operators';
+import {first, map} from 'rxjs/operators';
 import {PublicWorkshopsService} from '../../services/public-workshops/public-workshops.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LatestWorkshopGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  /**
+   * Returns an observable that either redirects to /id (where id is the id of the latest workshop),
+   * or redirects to /unknown.<br/>
+   * The latest workshop is determined by getting one emission from {@link publicWorkshopsService#workshops$},
+   * and taking the first item from that array.
+   */
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<UrlTree> {
     return this.publicWorkshopsService.workshops$.pipe(
-      take(1),
+      first(),
       map(workshops => {
         if (workshops.length === 0) return this.router.parseUrl('/unknown');
         return this.router.parseUrl(`/${workshops[0].id}`);
       })
     );
   }
+
 
   constructor(
     private readonly router: Router,
