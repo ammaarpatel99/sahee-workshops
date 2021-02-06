032685c07d2fc955e715e4e88b92a617458026ac
Ammaar Patel <ammaarpatel99@gmail.com>
2021-02-06 17:41:12 +0000

    refine promotional email component

--- projects/public/src/app/admin-workshop/admin-workshop-promotion/admin-workshop-promotion.component.ts
+++ projects/public/src/app/admin-workshop/admin-workshop-promotion/admin-workshop-promotion.component.ts
@@ -1,63 +1,93 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {Observable, of, Subscription} from 'rxjs';
-import {AdminWorkshop} from '../../../../../../functions/src/firebase-helpers/firestore-interfaces';
-import {filter, finalize, map, switchMap, take} from 'rxjs/operators';
+import {Component, Input} from '@angular/core';
+import {finalize, map} from 'rxjs/operators';
 import {FormControl, Validators} from '@angular/forms';
 import {EmailService} from '../../services/email/email.service';
+
+
+interface SentEmail {
+  message: string;
+  emailAddresses: string[];
+}
+
 
 @Component({
   selector: 'app-admin-workshop-promotion',
   templateUrl: './admin-workshop-promotion.component.html',
   styleUrls: ['./admin-workshop-promotion.component.scss']
 })
-export class AdminWorkshopPromotionComponent implements OnInit, OnDestroy {
-  @Input() workshop$: Observable<AdminWorkshop | undefined> = of(undefined);
-  readonly promotionEmail = new FormControl('', Validators.required);
-  sentEmail: {message: string; emailAddresses: string[]} | null = null;
+export class AdminWorkshopPromotionComponent {
+  /**
+   * Internal for {@link workshop}.
+   * @private
+   */
+  private _workshopID: string | null = null;
+  /**
+   * The workshop that the user is trying to send a message for.
+   */
+  @Input()
+  set workshopID(workshopID: string | null) {
+    if (workshopID) this.promotionEmail.reset();
+    this._workshopID = workshopID;
+  }
+  get workshop(): string | null {
+    return this._workshopID;
+  }
 
-  sendPromotionEmail(): void {
-    if (this.promotionEmail.pristine || this.promotionEmail.disabled || this.promotionEmail.invalid) {
+
+  /**
+   * Gets {@link workshop}, but throws an error if it is null.
+   */
+  getWorkshopID(): string {
+    const workshop = this.workshop;
+    if (!workshop) throw new Error(`No workshop to get.`);
+    return workshop;
+  }
+
+
+  /**
+   * Internal for {@link sentEmail}.
+   * @private
+   */
+  private _sentEmail: SentEmail | null = null;
+  /**
+   * The most recently sent email's details.
+   */
+  get sentEmail(): SentEmail | null {
+    return this._sentEmail;
+  }
+
+
+  /**
+   * The form control for the promotional email.
+   */
+  readonly promotionEmail = new FormControl('', Validators.required);
+
+
+  /**
+   * Sends the promotional email from {@link promotionEmail}.
+   */
+  sendPromotionEmail(): Promise<void> {
+    if (this.promotionEmail.disabled || this.promotionEmail.invalid) {
       throw new Error(`Can't send promotional email.`);
     }
-    this.workshop$.pipe(
-      take(1),
-      filter(workshop => !!workshop),
-      switchMap(workshop =>
-        this.emailService.promote$(
-          (workshop as AdminWorkshop).id, this.promotionEmail.value
-        )
-      ),
-      map(emailAddresses => {
-        this.sentEmail = {
-          emailAddresses,
-          message: this.promotionEmail.value
-        };
-        this.promotionEmail.reset('');
-      })
-    ).subscribe();
+    this.promotionEmail.disable();
+    const workshopID = this.getWorkshopID();
+    const message = this.promotionEmail.value;
+    return this.emailService
+      .promote$(workshopID, message)
+      .pipe(
+        map(res => {
+          this._sentEmail = {message, emailAddresses: res};
+        }),
+        finalize(() => {
+          this.promotionEmail.enable();
+          this.promotionEmail.reset();
+        })
+      ).toPromise();
   }
+
 
   constructor(
     private readonly emailService: EmailService
-  ) {
-    this.subscriptions.push(
-    );
-  }
-
-  private disableWhenLoading$(loading$: Observable<boolean>): Observable<void> {
-    return loading$.pipe(
-      map(loading => {
-        if (loading) this.promotionEmail.disable();
-        else this.promotionEmail.enable();
-      })
-    );
-  }
-
-  ngOnInit(): void { }
-
-  private readonly subscriptions: Subscription[] = [];
-  ngOnDestroy(): void {
-    for (const s of this.subscriptions) if (!s.closed) s.unsubscribe();
-  }
-
+  ) { }
 }
