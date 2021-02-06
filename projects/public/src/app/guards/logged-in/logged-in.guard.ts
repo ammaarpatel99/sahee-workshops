ed27bc5bf1232d78dfe5259e4da5dc72ad9bdb22
Ammaar Patel <ammaarpatel99@gmail.com>
2021-02-06 12:07:35 +0000

    refine guards

--- projects/public/src/app/guards/logged-in/logged-in.guard.ts
+++ projects/public/src/app/guards/logged-in/logged-in.guard.ts
@@ -11,44 +11,45 @@ import {
   Router
 } from '@angular/router';
 import { Observable } from 'rxjs';
-import {AngularFireAuth} from '@angular/fire/auth';
-import {map, take, tap} from 'rxjs/operators';
-import {UserService} from '../../services/user/user.service';
+import {first, map} from 'rxjs/operators';
+import {SignedInState, UserService} from '../../services/user/user.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedInGuard implements CanActivate, CanActivateChild, CanLoad {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<true | UrlTree> {
     return this.guard(state.url);
   }
-  canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<true | UrlTree> {
     return this.guard(state.url);
   }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canLoad(route: Route, segments: UrlSegment[]): Observable<true | UrlTree> {
     return this.guard(segments.join(''));
   }
 
+
+  /**
+   * If the user is signed in, the returned observable emits true.
+   * Otherwise it emits the {@link UrlTree} of the url from {@link UserService#signInUrl signInUrl(currentUrl)}.
+   * @param currentUrl - The url the user is trying to reach.
+   * @private
+   */
   private guard(currentUrl: string): Observable<true | UrlTree> {
-    return this.auth.user.pipe(
-      take(1),
-      map(user => {
-        if (user) return true;
+    return this.userService.userState$.pipe(
+      first(),
+      map(userState => {
+        if (userState.signedInState !== SignedInState.NO_USER) return true;
         const url = this.userService.signInUrl(currentUrl);
         return this.router.parseUrl(url);
       })
     );
   }
 
+
   constructor(
     private readonly userService: UserService,
-    private readonly auth: AngularFireAuth,
     private readonly router: Router
   ) { }
 }
